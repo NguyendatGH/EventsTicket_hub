bc2efbd2d2da623293123a080171bd1ff4f8b235
LePhuocHat <lephuochat@gmail.com>
2025-07-29 20:34:31 +0700

    update/ feedback feature

--- src/main/java/context/DBConnection.java
+++ src/main/java/context/DBConnection.java
@@ -13,9 +13,9 @@ import java.util.logging.Logger;
 public class DBConnection {
 
     private static final String DRIVER_NAME = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    private static final String DB_URL = "jdbc:sqlserver://172.16.0.170:1433;databaseName=EventTicketDB;encrypt=true;trustServerCertificate=true";
+    private static final String DB_URL = "jdbc:sqlserver://localhost:1433;databaseName=EventTicketDB;encrypt=true;trustServerCertificate=true";
     private static final String USER_DB = "sa"; 
-    private static final String PASS_DB = "Admin2404!"; 
+    private static final String PASS_DB = "123"; 
 
     private static final Logger LOGGER = Logger.getLogger(DBConnection.class.getName());
 
